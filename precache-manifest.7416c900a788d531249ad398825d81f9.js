66e692da077de996e84ef121e2da8aacb4aa26c0
gauravkr2506 <gauravkr2506@gmail.com>
2020-04-06 16:23:10 +0530

    Updates

---
+++ precache-manifest.7416c900a788d531249ad398825d81f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5176a5a436ffc19e267dd5e50ad7dfd",
+    "url": "/snake-game-react/index.html"
+  },
+  {
+    "revision": "037a46358e30ad053fb1",
+    "url": "/snake-game-react/static/css/main.15463aaa.chunk.css"
+  },
+  {
+    "revision": "06d97cad5335b39e5b25",
+    "url": "/snake-game-react/static/js/2.e6dfa101.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game-react/static/js/2.e6dfa101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037a46358e30ad053fb1",
+    "url": "/snake-game-react/static/js/main.4de2bb90.chunk.js"
+  },
+  {
+    "revision": "77d80cd0d2d420a14c3a",
+    "url": "/snake-game-react/static/js/runtime-main.4c3c52cb.js"
+  }
+]);
